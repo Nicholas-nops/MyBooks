2df2eca1068a292767f29b76fa3add024b6f1de0
Nicholas-nops <nicholaspacidonio@gmail.com>
2021-03-09 10:38:41 -0300

    Error on send books to db corrected

--- frontend/src/components/Books/willread/WillRead.js
+++ frontend/src/components/Books/willread/WillRead.js
@@ -1,84 +1,98 @@
-import React, { useState,useEffect } from 'react';
-import './willread.css';
-import 'material-icons';
-import 'materialize-css'
-import Card from '../../card/card';
-import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
-import { storage,ref } from '../../../firebase/firebase';
+import React, { useState } from "react";
+import "./willread.css";
+import "material-icons";
+import "materialize-css";
+import Card from "../../card/card";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import { storage } from "../../../firebase/firebase";
 
 export default function WillRead() {
-    const [modal, showModal] = useState(false);
-    const [bookCover, setCover] = useState(null);
-    const [bookTitle, setTtile] = useState('');
-    const [bookDesc, setDesc] = useState('');
-    const [bookUrl, setUrl] = useState('');
-    
-    useEffect(() => {
-        
-  
-    }, [bookUrl,bookCover])
-    const handleImg = () => {
-        let imgID = uuidv4();
+  const [modal, showModal] = useState(false);
+  const [bookCover, setCover] = useState(null);
+  const [bookTitle, setTtile] = useState("");
+  const [bookDesc, setDesc] = useState("");
+
+  const handleImg = () => {
+    let imgID = uuidv4();
 
     if (bookCover === "" && bookTitle === "" && bookDesc === "") {
-
-        alert("Some of the fields are empty");
-    }else {
-        const uploadTask = storage.ref(`images/${imgID}`).put(bookCover);
-            uploadTask.on(
-                'state_changed',
-                snapshot => { },
-                error => {
-                    console.log(error);
-                },
-                () => {
-                    storage
-                        .ref('images')
-                        .child(imgID)
-                        .getDownloadURL()
-                        .then( url => {
-                            setUrl(url)
-                        })
-                }
-                
-            );
-            
+      alert("Some of the fields are empty");
+    } else {
+      const uploadTask = storage.ref(`images/${imgID}`).put(bookCover);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {},
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(imgID)
+            .getDownloadURL()
+            .then((url) => {
+              let bookData = {
+                bookCover: url,
+                bookTitle: bookTitle,
+                bookDesc: bookDesc,
+              };
+              axios.post("http://localhost:3002/api/insert", bookData);
+            });
         }
-            handleUpload();
-        }    
-
-    const handleUpload = async () => { 
-        let bookData = {
-            "bookCover": bookUrl,
-            "bookTitle": bookTitle,
-            "bookDesc": bookDesc,
-        }
-        axios.post('http://localhost:3002/api/insert', bookData)
+      );
     }
-    return (
-        <div className='modal__container'>
-            <Card />
-            <button className="modal__open__button" onClick={() => (showModal(modal ? false : true))} ><span className="material-icons btn3">library_add</span></button>
-            {
-                modal ?
-                    <div className='modal__body'>
-                        <div className="modal__content">
-                            Book cover: <input placeholder="Book ISBN" onChange={(e) =>  setCover(e.target.files[0])} type="file"/>
-                            <input placeholder="Book Title" onChange={(e) => setTtile(e.target.value)} type="text" id="bookTitle" className="modal__book__title" />
-                            <textarea placeholder="Short description" onChange={(e) => setDesc(e.target.value)} id="bookDesc" type="text" className="modal__book__description"></textarea>
-                            <input className='modal__btn__send' onClick={handleImg} type="submit" value="add" />
-                            <button className='modal__btn_close' onClick={() => (showModal(false))}>Cancel</button>
-                            {bookUrl}
-                        </div>
-                    </div>
-                    :
-                    ''
-            }
+  };
+
+  return (
+    <div className="modal__container">
+      <Card />
+      <button
+        className="modal__open__button"
+        onClick={() => showModal(modal ? false : true)}
+      >
+        <span className="material-icons btn3">library_add</span>
+      </button>
+      {modal ? (
+        <div className="modal__body">
+          <div className="modal__content">
+            Book cover:{" "}
+            <input
+              placeholder="Book ISBN"
+              onChange={(e) => setCover(e.target.files[0])}
+              type="file"
+            />
+            <input
+              placeholder="Book Title"
+              onChange={(e) => setTtile(e.target.value)}
+              type="text"
+              id="bookTitle"
+              className="modal__book__title"
+            />
+            <textarea
+              placeholder="Short description"
+              onChange={(e) => setDesc(e.target.value)}
+              id="bookDesc"
+              type="text"
+              className="modal__book__description"
+            ></textarea>
+            <input
+              className="modal__btn__send"
+              onClick={handleImg}
+              type="submit"
+              value="add"
+            />
+            <button
+              className="modal__btn_close"
+              onClick={() => showModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-    )
-
-
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
-
-
--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -6,7 +6,6 @@ import './card.css'
 
 export default function Card() {
     const [bookList, setBookList] = useState([]);
-    const books = [];
 
     useEffect(() => {
         axios.get('http://localhost:3002/api/get').then(res => {
@@ -19,16 +18,16 @@ export default function Card() {
         <div>
             {bookList.map((book) => {
                 return (
-                    <div className="card">
+                    <div className="card" key={book.ID}>
                         <img src={book.bookCover} alt="capa" style={style.img}></img>
                        
                             <h4><b>{book.bookTitle}</b></h4>
                             <p>{book.bookDesc}</p>
                             <div>
-                            <button className='btnReaded'>Alredy read <span className=""><img src={addIcon}></img></span></button>
+                            <button className='btnReaded'>Alredy read <span className=""><img alt="add" src={addIcon}></img></span></button>
                             </div>
                             <div>
-                            <button className='btnDelete'>Delete <span className=""><img src={removeIcon}></img></span></button>
+                            <button className='btnDelete'>Delete <span className=""><img alt="add" src={removeIcon}></img></span></button>
                             </div>
           
                     </div>
